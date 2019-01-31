e037c58d6747992a899ea92e0708bac5f338e099
Tukmen Mogoria <tukmogi@gmail.com>
2019-01-31 16:58:09 +0300

    [Finished #163539139]created a page for admin to add, edit and delete political parties

---
+++ UI/scripts/main.js
@@ -0,0 +1,11 @@
+// console.log(document.links);
+// var active = 0;
+// for (var i = 0; i < document.links.length; i++) {
+//     console.log("document url: " + document.URL);
+//     console.log("link url: " + document.links[i].href);
+//     if (document.links[i].href === document.URL) {
+//         active = i;
+//     }
+// }
+// document.links[active].className = 'active';
+
