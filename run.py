dfc0c461f5eeeb5008f8b1bcd99b75aef07c7541
Tukmen Mogoria <tukmogi@gmail.com>
2019-02-05 23:12:33 +0300

    [Finishes #163740684] fix module import that prevented flask from running

--- run.py
+++ run.py
@@ -1,12 +1,8 @@
 from app import create_app
-from instance.config import app_config
 
 
 flask_app = create_app('development')
 
-@flask_app.route("/")
-def say_hello():
-    return "Hello world"
 
 if __name__ == '__main__':
-    flask_app.run()
+    flask_app.run(debug=True)
