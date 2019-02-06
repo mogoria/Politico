4dc264c68e4b6295c7a22a4ceac198e472bbbb3c
Tukmen Mogoria <tukmogi@gmail.com>
2019-02-06 03:24:42 +0300

    [Finishes #163714085] created model class for political parties

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,9 @@ from instance.config import app_config
 from app.api.v1.views import v1_bp
 from app.api.v1.views.political_party_route import v1_bp as political_party_bp
 
-
+flask_app = Flask(__name__)
 def create_app(config_name):
     """Returns flask app based on the configuration"""
-    flask_app = Flask(__name__)
     flask_app.config.from_object(app_config[config_name])
     flask_app.register_blueprint(v1_bp)
     flask_app.register_blueprint(political_party_bp)
