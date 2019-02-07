e5d91d57c971416ae0cb6771809051ab55a131e6
Tukmen Mogoria <tukmogi@gmail.com>
2019-02-07 17:27:22 +0300

    [Finishes #163785522] added base test]

---
+++ app/tests/test_home.py
@@ -0,0 +1,10 @@
+from . import BaseTest
+
+
+class TestHome(BaseTest):
+    """class to test home route"""
+    def test_home_route(self):
+        """tests response of home route"""
+        resp = self.client.get(path="/api/v1/")
+        self.assertEqual(resp.status_code, 200)
+    
