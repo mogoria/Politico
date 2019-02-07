a11e3e1de372db1f1ab7168a3907e8edf94f6ef5
Tukmen Mogoria <tukmogi@gmail.com>
2019-02-08 02:12:18 +0300

    [Bug #163803470] added mock party data

--- app/tests/test_parties.py
+++ app/tests/test_parties.py
@@ -5,7 +5,18 @@ import json
 class BasePartiesTest(BaseTest):
     path = "/api/v1/parties"
 
-    
+    party_data = {
+            "name":"new party",
+            "id":254,
+            "photoUrl":"https://photos/254",
+            "hqAddress": "Nairobi"
+        }
+    party_data2 = {
+            "name":"new party2",
+            "id":257,
+            "logoUrl":"https://photos/257",
+            "hqAddress": "Somewhere"
+        }
         
     
 class TestPartiesStatusCodes(BasePartiesTest):
