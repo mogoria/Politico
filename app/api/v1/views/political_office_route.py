b2effcf9f7f4683e1c18cdeeac5317715aad63b5
Tukmen Mogoria <tukmogi@gmail.com>
2019-02-10 02:17:47 +0300

    [Chore #163831171] refactored code

--- app/api/v1/views/political_office_route.py
+++ app/api/v1/views/political_office_route.py
@@ -8,39 +8,44 @@ from . import utils
 OFFICE = PoliticalOffice()
 @v1_bp.route("/offices", methods=['POST'])
 def create_offices():
+    """creates an office and adds to the list of offices in OFFICE object
+        first checks whether it exists
+    """
     data = request.get_json(force=True)
-    valid_fields = ["id", "type", "name"]
     sample_data = {
         "id":12,
         "type": "office type",
         "name": "office name"
     }
+    valid_fields = sample_data.keys()
     response = {}
 
-    if utils.check_valid_fields(data, valid_fields):
-        if not utils.check_valid_type(data, sample_data):
-            created_office = OFFICE.create_office(**data)
-            if created_office:
-                return utils.util_response(201, created_office)
-            return utils.util_response(409, "office already exists")
-        else:
-            response = {
-                "status":400,
-                "error": "incorrect format, please provide valid types for. {}".format(
-                    ", ".join(
-                        ["{}:{}".format(key, type(value)) for key, value in sample_data.items()]
-                        )
-                    )
-            }
-    else:
+    if utils.check_valid_fields(data, valid_fields) is False:
         response = {
             "status":400,
-            "error": "incorrect format, please provide valid fields. {}".format(", ".join(valid_fields)) 
+            "error": "incorrect format, please provide valid fields. {}"
+                     .format(", ".join(valid_fields))
         }
-    return utils.util_response(response.get('status'), response.get('error'))
+    if (response == {}) and utils.check_valid_type(data, sample_data):
+        response = {
+            "status":400,
+            "error": "incorrect format, please provide valid types for. {}".format(
+                ", ".join(
+                    ["{}:{}".format(key, type(value)) for key, value in sample_data.items()]
+                    )
+                )
+        }
+    if response != {}:
+        return utils.util_response(response.get('status'), response.get('error'))
+
+    created_office = OFFICE.create_office(**data)
+    if created_office:
+        return utils.util_response(201, created_office)
+    return utils.util_response(409, "office already exists")
 
 @v1_bp.route("/offices", methods=['GET'])
 def get_all_offices():
+    """endpoint to make a get request for offices"""
     offices = OFFICE.Offices
     if offices:
         return make_response(
@@ -52,6 +57,7 @@ def get_all_offices():
 
 @v1_bp.route("/offices/<int:office_id>", methods=['GET'])
 def get_single_office(office_id):
+    """endpoint to make a get request for only one office"""
     found_office = [office for office in OFFICE.Offices if office['id'] == office_id]
     if found_office:
         return utils.util_response(200, found_office)
